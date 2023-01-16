4752604786d8e58d311b3edd2ea0df71149c4c16
Han Keat Goh <hankeat.goh@gmail.com>
2023-01-16 16:29:53 +0800

    first commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("this is new string");
+        System.out.println("this is new string for jenkins");
     }
 
     public String getMessage() {
