083ec6ee1f5a529749d6364710cfe845dee36ea6
Han Keat Goh <hankeat.goh@gmail.com>
2023-01-17 09:36:41 +0800

    new change second day 3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("this is new string for new day 2");
+        System.out.println("this is new string for new day 3");
     }
 
     public String getMessage() {
